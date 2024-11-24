6cc1cc58183edee845aa1f2628ac6906c755ed9f
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-24 17:17:02 -0300

    Configurando o React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/login' element {<Login />} />
+        <Route path='/register' element {<Cadastro />} /> */}
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
