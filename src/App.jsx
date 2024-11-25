77bea8356dc9757495a7397fc1d31093d6785029
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-24 22:20:01 -0300

    Pagina de busca com Search Params

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 import Navbar from './components/Navbar'
+import SearchForm from './components/SearchForm'
 
 import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
 import Info from './pages/Info'
 import NotFound from './pages/NotFound'
+import Search from './pages/Search'
 
 function App() {
   return (
@@ -16,12 +18,14 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
       <Navbar />
+      <SearchForm />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products/:id' element={<Product />} />
         <Route path='/products/:id/info' element={<Info />} />
         <Route path='/about' element={<About />} />
         <Route path="*" element={<NotFound />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
       </BrowserRouter>
     </div>
